1d741875864e5e7e693099e603f341232b40b644
super <yunshijyz@163.com>
2021-12-20 15:05:31 +0800

    modife

--- src/node/NodeDemo.java
+++ src/node/NodeDemo.java
@@ -5,10 +5,13 @@ package node;
 
 public class NodeDemo {
 
+    //头节点指针
     private Node head;
 
+    //尾节点指针
     private Node last;
 
+    //链表实际长度
     private int size;
 
 
@@ -25,15 +28,19 @@ public class NodeDemo {
         Node insertNode = new Node(data);
 
         if(size == 0){
+            //空链表
             head = insertNode;
             last = insertNode;
         } else if (index == 0){
+            //插入头部
             insertNode.next = head;
             head = insertNode;
         } else if (size == index){
+            //插入尾部
             last.next = insertNode;
             last = insertNode;
         } else {
+            //插入中间
             Node prevNode = get(index-1);
             insertNode.next = prevNode.next;
             prevNode.next = insertNode;
@@ -43,6 +50,46 @@ public class NodeDemo {
 
     }
 
+
+    /**
+     * 删除元素
+     * @param index
+     * @return
+     */
+    public Node remove(int index){
+        if(index<0 || index >=size){
+            throw new IndexOutOfBoundsException("超出节点范围");
+        }
+
+        Node removeNode = null;
+        if(index == 0){
+            //删除头节点
+            removeNode = head;
+            head = head.next;
+        } else if(index == size-1){
+            //删除尾节点
+            Node prevNode = get(index-1);
+            removeNode = prevNode.next;
+            last = prevNode;
+        } else {
+            //删除中间节点
+            Node prevNode = get(index-1);
+            Node nextNode = prevNode.next.next;
+            removeNode = prevNode.next;
+            prevNode.next = nextNode;
+        }
+
+        size--;
+
+        return removeNode;
+    }
+
+
+    /**
+     * 链表查找元素
+     * @param index
+     * @return
+     */
     public Node get(int index){
         if(index<0 || index >=size){
             throw new IndexOutOfBoundsException("超出节点范围");
@@ -82,6 +129,8 @@ public class NodeDemo {
         nodeDemo.insert(2,9);
         nodeDemo.insert(3,5);
 
+
+        nodeDemo.remove(2);
         nodeDemo.output();
     }
 
